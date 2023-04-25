cfc194487a947ced56970372024c8697562cd656
calint <calin.tenitchi@gmail.com>
2023-04-25 15:59:01 +0200

    .

--- znxrc.srcs/sources_1/Registers.v
+++ znxrc.srcs/sources_1/Registers.v
@@ -4,7 +4,7 @@ module Registers(
     input [3:0] ra2,
     input we, // write 'wd' to address 'ra1'
     input [15:0] wd, // data to write when 'we' is true
-    input inca, // if true increases value of ra1
+    input inca, // if true increases value of 'ra1' after the read or write is done
     output reg [15:0] rd1,
     output reg [15:0] rd2
     );
@@ -12,12 +12,14 @@ module Registers(
 reg [15:0] regs [0:15];
 
 always @(posedge clk) begin
-  rd1 <= regs[ra1];
-  rd2 <= regs[ra2];
-  if (inca)
-    regs[ra1] <= regs[ra1] + 1;
   if (we)
-    regs[ra1] <= wd;
+    regs[ra1] = wd;
+  else begin
+    rd1 = regs[ra1];
+    rd2 = regs[ra2];
+  end
+  if (inca)
+    regs[ra1] = regs[ra1] + 1;
 end
 
 endmodule
