adc86e6c6441f417ef691dd1060295427a7b16f8
calint <calin.tenitchi@gmail.com>
2023-04-27 20:18:38 +0200

    .

--- znxrc.srcs/sources_1/Control.v
+++ znxrc.srcs/sources_1/Control.v
@@ -14,8 +14,8 @@ reg [3:0] reg_to_write = 0;
 
 wire cs_zf,cs_nf,alu_zf,alu_nf;
 wire [15:0] alu_res;
-wire [15:0] reg1_val;
-wire [15:0] reg2_val;
+wire [15:0] reg1_dat;
+wire [15:0] reg2_dat;
 wire ls_loop_finished;
 wire [15:0] ls_jmp_address;
 wire [15:0] cs_program_counter_nxt;
@@ -29,8 +29,8 @@ wire [3:0] op = state == 0 ? instruction[7:4] : 0;
 wire [3:0] reg1 = instruction[11:8];
 wire [3:0] reg2 = state == 1 ? reg_to_write : instruction[15:12];
 wire [2:0] alu_op = instruction[7:5] == 3'b011 && reg1 == 0 ? 3'b111 : instruction[7:5];
-wire [15:0] alu_operand_1 = alu_op == 3'b011 && reg1 != 0 ? {{12{reg1[3]}}, reg1} : reg1_val;
-//wire [15:0] alu_operand_1 = reg1_val;
+wire [15:0] alu_operand_1 = alu_op == 3'b011 && reg1 != 0 ? {{12{reg1[3]}}, reg1} : reg1_dat;
+//wire [15:0] alu_operand_1 = reg1_dat;
 wire [9:0] imm10 = state == 0 ? instruction[15:6] : 0;
 wire is_alu_op = op == 4'b1010 || op == 4'b0010 || op == 4'b0110; // 'add','inc','shf' or 'not':
 wire regs_we = state == 1 || is_alu_op ? 1 : 0;
@@ -92,7 +92,7 @@ LoopStack ls(
     .rst(rst),
     .clk(clk),
     .new_loop(ls_new_loop), // true to create a new loop using 'loop_address' for the jump and 'count' for the number of iterations
-    .count(reg1_val), // number of iterations in loop when creating new loop with 'new_loop'
+    .count(reg1_dat), // number of iterations in loop when creating new loop with 'new_loop'
     .loop_address(program_counter), // the address to which to jump at next
     .next(ls_next), // true if current loop is at instruction that is 'next'
     .loop_finished(ls_loop_finished), // true if current loop is finished
@@ -105,15 +105,14 @@ Registers regs(
     .ra2(reg2),
     .we(regs_we), // write 'wd' to address 'ra1'
     .wd(regs_wd), // data to write when 'we' is true
-    .rd1(reg1_val),
-    .rd2(reg2_val)
+    .rd1(reg1_dat),
+    .rd2(reg2_dat)
     );
 
 ALU alu(
-//    .clk(clk),
     .op(alu_op),
     .a(alu_operand_1),
-    .b(reg2_val),
+    .b(reg2_dat),
     .result(alu_res),
     .zf(alu_zf),
     .nf(alu_nf)
