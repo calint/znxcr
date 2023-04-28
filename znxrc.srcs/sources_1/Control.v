db6ae552185f3735d0749c8e0df5f1d4f0fa2d1a
calint <calin.tenitchi@gmail.com>
2023-04-28 23:01:01 +0200

    .

--- znxrc.srcs/sources_1/Control.v
+++ znxrc.srcs/sources_1/Control.v
@@ -52,9 +52,9 @@ wire cs_push = is_cs_op ? instr_c : 0; // enabled if command is 'call'
 wire cs_pop = is_cs_op ? instr_r : 0; // enabled if command also does 'return'
 
 wire is_alu_op = op == OP_ADD || op == OP_ADDI || op == OP_SHIFT;
-wire [2:0] alu_op = op == OP_SHIFT && reg1 == 0 ? ALU_NOT : // 'shift' with argument 0 is interpreted as a 'not'
-                    op == OP_ADDI ? ALU_ADD : // 'addi' is add with 'reg1' as a signed value instead of regs[reg1]
-                    op; // same as op
+wire [2:0] alu_op =         op == OP_SHIFT && reg1 == 0 ? ALU_NOT : // 'shift' with argument 0 is interpreted as a 'not'
+                            op == OP_ADDI ? ALU_ADD : // 'addi' is add with 'reg1' as a signed value instead of regs[reg1]
+                            op; // same as op
 wire [15:0] alu_operand_1 = op == OP_SHIFT && reg1 != 0 ? {{12{reg1[3]}}, reg1} : // 'shift' with argument signed value of 'reg1' argument
                             op == OP_ADDI ? {{12{reg1[3]}}, reg1} : // 'addi' is add with 'reg1' argument as signed value instead of regs[reg1]
                             reg1_dat; // otherwise regs[reg1]
