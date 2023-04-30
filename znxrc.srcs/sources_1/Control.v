7c69c96eb4c3e364bce7f5cb8e57e44809984f39
calint <calin.tenitchi@gmail.com>
2023-04-30 22:26:48 +0200

    .

--- znxrc.srcs/sources_1/Control.v
+++ znxrc.srcs/sources_1/Control.v
@@ -21,7 +21,7 @@ localparam ALU_ADD = OP_ADD;
 localparam ALU_SHIFT = OP_SHIFT;
 localparam ALU_NOT = 3'b111;
 
-reg state; // 0 => normal instruction, 1 => write the instruction to register 'reg_to_write'
+reg is_loadi; // enabled if instruction data copy to register 'reg_to_write'
 reg [15:0] pc; // program counter
 reg [15:0] cs_pc_in; // program counter as input to call stack
 wire [15:0] cs_pc_out; // program counter at top of the call stack
@@ -39,12 +39,12 @@ wire instr_r = instr[3]; // if enabled execute command and return from current s
 wire instr_c = instr[4]; // if enabled call a sub-routine (instr_r && instr_c is illegal and instead enables more commands)
 wire [3:0] op = instr[7:5];
 wire [3:0] rega = instr[11:8];
-wire [3:0] regb = state == 1 ? reg_to_write : instr[15:12];
+wire [3:0] regb = is_loadi ? reg_to_write : instr[15:12];
 wire [7:0] imm8 = instr[15:8];
 wire [10:0] imm11 = instr[15:5];
 
 wire ls_done; // loop stack enables this if it is the last iteration in current loop
-wire ls_new_loop = state == 0 && instr[11:0] == OP_LOOP; // creates new loop with counter set from regs[regb]
+wire ls_new_loop = !is_loadi && instr[11:0] == OP_LOOP; // creates new loop with counter set from regs[regb]
 wire [15:0] ls_pc_out; // loop stack: address to set 'pc' to if loop is not done
 
 wire is_cr = instr_c && instr_r; // enabled if illegal c && r op => enables 8 other commands that can't piggy back 'return'
@@ -52,8 +52,7 @@ wire is_cs_op = is_do_op && !is_cr && (instr_c ^ instr_r); // enabled if command
 wire cs_push = is_cs_op ? instr_c : 0; // enabled if command is 'call'
 wire cs_pop = is_cs_op ? instr_r : 0; // enabled if command also does 'return'
 
-// note. state==0 is tested in the is_cs_op and then propagated through the ands
-wire is_alu_op = !is_cr && !cs_push && (op == OP_ADD || op == OP_ADDI || op == OP_COPY || op == OP_SHIFT);
+wire is_alu_op = !is_loadi && !is_cr && !cs_push && (op == OP_ADD || op == OP_ADDI || op == OP_COPY || op == OP_SHIFT);
 wire [2:0] alu_op = op == OP_SHIFT && rega == 0 ? ALU_NOT : // 'shift' 0 interpreted as 'not'
                     op == OP_ADDI ? ALU_ADD : // 'addi' is add with signed immediate value 'rega'
                     op; // same as op
@@ -65,15 +64,15 @@ wire cs_zf,cs_nf,alu_zf,alu_nf,zf,nf;
 wire zn_we = is_alu_op || cs_pop; // update flags if alu op or return
 wire zn_sel = is_alu_op; // if alu op then enabled otherwise it is 'cs_pop'
 
-wire is_do_op = state==0 && ((instr_z && instr_n) || (zf==instr_z && nf==instr_n));
+wire is_do_op = !is_loadi && ((instr_z && instr_n) || (zf==instr_z && nf==instr_n));
 
 wire ram_we = op == OP_STORE; // connected to ram write enable input
 wire [15:0] ram_dat_out; // connected to ram data output
 
 // enables write to registers
-wire regs_we = state == 1 || (is_do_op && (is_alu_op || op == OP_LOAD));
+wire regs_we = is_loadi || (is_do_op && (is_alu_op || op == OP_LOAD));
 // data written to 'regb' if 'regs_we' is enabled
-wire [15:0] regs_wd = state == 1 ? instr : // write instruction into registers
+wire [15:0] regs_wd = is_loadi ? instr : // write instruction into registers
                       is_alu_op ? alu_res : // write alu result to registers
                       op == OP_LOAD ? ram_dat_out : // write ram data output to registers
                       0; // otherwise don't write to registers
@@ -87,12 +86,12 @@ end
 
 always @(posedge clk) begin
     if (rst) begin
-        state <= 0;
+        is_loadi <= 0;
         pc <= -1;
     end else begin
-        case(state)
+        case(is_loadi)
         //---------------------------------------------------------------------
-        0: // state 0: decode instruction
+        0: // instruction
         //---------------------------------------------------------------------
         begin
             if (cs_push) begin // 'call': calls imm11<<3
@@ -104,34 +103,33 @@ always @(posedge clk) begin
                     case(op)
                     //-------------------------------------------------------------
                     OP_LOADI: begin // load register with data from the next instruction 
-                        state <= 1;
+                        is_loadi <= 1;
                         reg_to_write <= regb;
                     end
                     //-------------------------------------------------------------
                     OP_SKIP: begin
-                        state <= 0;
+                        is_loadi <= 0;
                         if (is_do_op) begin
                             pc <= pc + {8'd0, imm8};
                         end
                     end
                     //-------------------------------------------------------------
-                    default: state <= 0;
+                    default: is_loadi <= 0;
                     endcase
                 end else begin // instruction bits c and r are not 11
-                    state <= 0;
+                    is_loadi <= 0;
                 end
                 // if loop 'next' 
-                // ? racing with LoopStack that may change 'ls_pc_out' and 'ls_done' during its 'posedge clk'
                 if (is_do_op && instr_x && !ls_done) begin
                     pc <= ls_pc_out;        
                 end        
             end
         end
         //---------------------------------------------------------------------
-        1: // state 1: write to 'reg_to_write' current 'instruction'
+        1: // write instruction data to 'reg_to_write'
         //---------------------------------------------------------------------
         begin
-            state <= 0;
+            is_loadi <= 0;
         end
 //        default: state = 0;
         endcase
