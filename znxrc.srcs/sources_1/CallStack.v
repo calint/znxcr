1ed6a09855404e7828ce674b66eb36ef28851370
calint <calin.tenitchi@gmail.com>
2023-05-03 14:12:22 +0200

    .

--- znxrc.srcs/sources_1/CallStack.v
+++ znxrc.srcs/sources_1/CallStack.v
@@ -8,27 +8,25 @@ module CallStack(
     input nf_in, // current negative flag
     input push,
     input pop,
-    output [15:0] pc_out, // popped program counter
-    output zf_out, // popped zero flag
-    output nf_out // popped negative flag
+    output reg [15:0] pc_out, // popped program counter
+    output reg zf_out, // popped zero flag
+    output reg nf_out // popped negative flag
     );
     
     reg [3:0] mem_idx;
     reg [17:0] mem [0:15];
     
-    assign zf_out = mem[mem_idx][17];
-    assign nf_out = mem[mem_idx][16];
-    assign pc_out = mem[mem_idx][15:0];
-        
     always @(posedge clk) begin
         $display("  clk: CallStack");
         if (rst) begin
             mem_idx <= 4'hf;
         end else begin
+            zf_out = mem[mem_idx][17];
+            nf_out = mem[mem_idx][16];
+            pc_out = mem[mem_idx][15:0];
             if (push) begin
                 mem_idx = mem_idx + 1;
                 mem[mem_idx] = {zf_in, nf_in, pc_in};
- //               $display(" cs:push: [%d]",mem_idx,pc_in);
             end else if (pop) begin
                 mem_idx = mem_idx - 1;
             end
