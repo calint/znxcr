c994e6784c09faa37aad923a2031c79fbe084fb8
calint <calin.tenitchi@gmail.com>
2023-05-07 00:14:27 +0200

    .

--- znxrc.srcs/sources_1/System.v
+++ znxrc.srcs/sources_1/System.v
@@ -2,23 +2,23 @@
 
 module System(
     input reset,
-    input clk_in1,
+    input clk_in_100MHz,
     output debug1
     );
 
-wire clk_out;
 wire clk_locked;
+wire clk_out_33MHz;
 
-Clock clk(
+Clocks clks(
     .reset(reset),
     .locked(clk_locked),
-    .clk_in1(clk_in1),
-    .clk_out1(clk_out)
+    .clk_in_100MHz(clk_in_100MHz),
+    .clk_out_33MHz(clk_out_33MHz)
 );
 
 Control ctrl(
     .rst(!clk_locked),
-    .clk(clk_out),
+    .clk(clk_out_33MHz),
     .debug1(debug1)
 );
 
