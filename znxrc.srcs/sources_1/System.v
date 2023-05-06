68934137ba02dbaf67ab83eb122011fbd6595eca
calint <calin.tenitchi@gmail.com>
2023-05-06 21:38:27 +0200

    .

--- znxrc.srcs/sources_1/System.v
+++ znxrc.srcs/sources_1/System.v
@@ -7,15 +7,17 @@ module System(
     );
 
 wire clk_out;
+wire clk_locked;
 
 Clock clk(
     .reset(rst),
+    .locked(clk_locked),
     .clk_in1(clk_100MHz),
     .clk_out1(clk_out)
 );
 
 Control ctrl(
-    .rst(rst),
+    .rst(!clk_locked),
     .clk(clk_out),
     .debug1(debug1)
 );
